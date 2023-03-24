891ffc116b4f8981f7e37bf8845547b1745f237e
Zahrddin <zahriddin@test.com>
2023-03-24 19:08:42 +0500

    next js repository

---
+++ app/newpage/page.js
@@ -0,0 +1,9 @@
+function newpage () {
+    return ( 
+        <div className="text-2xl font-bold">
+            New Page
+        </div>
+     );
+}
+
+export default newpage ;
--- app/src/header.js
+++ app/src/header.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 function Header() {
     return ( 
-        <div className="bg-emerald-500 p-2 m-10 flex justify-around font-semibold rounded-full">
+        <div className="bg-yellow-500 p-2 m-10 flex justify-around font-semibold rounded-full">
             <Link className="bg-white border rounded-full p-2" href={"/"}>Home</Link>
             <Link className="bg-white border rounded-full p-2" href={"/sardor"}>Sardor</Link>
             <Link className="bg-white border rounded-full p-2" href={"/sardor/youtube"}>Youtube</Link>
+            <Link className="bg-white border rounded-full p-2" href={"/newpage"}>NewPage</Link>
+            
         </div>
      );
 }
